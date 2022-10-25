3e950ff9503e6dd36147eaf10b5ba40d38e4708f
petrovyuri <mr.developer@yandex.ru>
2022-10-25 13:53:57 +0300

    Вторая кнопка

--- lib/ui/counter_screen.dart
+++ lib/ui/counter_screen.dart
@@ -23,30 +23,49 @@ class _CounterScreenView extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Flutter Demo Home Page"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  BlocBuilder<CounterBloc, CounterState>(
+                    builder: (context, state) {
+                      return Text(
+                        state.counter.toString(),
+                        style: Theme.of(context).textTheme.headline4,
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
-            BlocBuilder<CounterBloc, CounterState>(
-              builder: (context, state) {
-                return Text(
-                  state.counter.toString(),
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              },
+            Align(
+              alignment: Alignment.bottomRight,
+              child: FloatingActionButton(
+                onPressed: () =>
+                    context.read<CounterBloc>().add(CounterIncrementedEvent()),
+                tooltip: 'Increment',
+                child: const Icon(Icons.add),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: FloatingActionButton(
+                onPressed: () =>
+                    context.read<CounterBloc>().add(CounterDecrementedEvent()),
+                tooltip: 'Increment',
+                child: const Icon(Icons.remove),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () =>
-            context.read<CounterBloc>().add(CounterIncrementedEvent()),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
